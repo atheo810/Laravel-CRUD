a409acc93851c6de0463be80ae3b8d7d455969d1
atheo810 <atheos810@gmail.com>
2023-11-19 14:01:44 +0800

    feat: protected and hidden attributes Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,19 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\DBAL\TimestampType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Book extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     const CREATED_AT = 'created_at';
     const UPDATED_AT = 'updated_at';
 
 
     protected $fillable = ['book_name', 'description', 'category_id'];
+
+    protected $attributes = [
+        'is_deleted' => false,
+    ];
+
+    protected $hidden = [
+        'is_deleted'
+    ];
 
     public function category()
     {
