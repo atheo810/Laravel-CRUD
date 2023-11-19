f74f4f75c776d1781e3d8bd551f2f73fcbb9b26b
atheo810 <atheos810@gmail.com>
2023-11-19 14:00:45 +0800

    feat: Book index API

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     //
+    public function index()
+    {
+        $book = Book::all();
+        return response()->json([
+            'Success' => 'true',
+            'message' => 'Book List',
+            'data' => $book
+        ]);
+    }
 }
