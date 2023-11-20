6d1e37a10f4a62e8073f0332662778f92aa00245
atheo810 <atheos810@gmail.com>
2023-11-20 15:34:35 +0800

    feat: api Book

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\BookCategoryController;
-use App\Http\Controllers\BookController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,11 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('book', [BookController::class, 'index']);
-
-Route::post('book/create', [BookController::class, 'create']);
-
 Route::prefix('bookCategory')
     ->name('bookCategory')
     ->group(function () {
@@ -33,4 +28,14 @@ Route::prefix('bookCategory')
         Route::get('/{bookCategory}', [BookCategoryController::class, 'show'])->name('show');
         Route::put('/{bookCategory}', [BookCategoryController::class, 'update'])->name('update');
         Route::delete('/{bookCategory}', [BookCategoryController::class, 'destroy'])->name('destroy');
+    });
+
+Route::prefix('book')
+    ->name('book')
+    ->group(function () {
+        Route::get('/', [BookController::class, 'index'])->name('index');
+        Route::post('/', [BookController::class, 'store'])->name('store');
+        Route::get('/{book}', [BookController::class, 'show'])->name('show');
+        Route::put('/{book}', [BookController::class, 'update'])->name('update');
+        Route::delete('/{book}', [BookController::class, 'destroy'])->name('destroy');
     });
